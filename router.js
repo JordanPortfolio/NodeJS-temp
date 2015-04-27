03530c2c73e3c1ddf595802e85092b3c48846f2e
Jordan Simpson <jordan@jordanportfolio.com>
2015-04-26 17:03:09 -0700

    Searching works and redirects the user to a page that displays the users image, username, how many badges, and amount of JavaScript points

--- router.js
+++ router.js
@@ -1,17 +1,34 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
+var querystring = require("querystring");
+
 var commonHeaders = {'Content-Type': 'text/html'};
 
 // Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
     // if url == "/" && GET
     if (request.url === "/") {
-       // show search
-       response.writeHead(200, commonHeaders);
-       renderer.view("header", {}, response);
-       renderer.view("search", {}, response);
-       renderer.view("footer", {}, response);
-       response.end();
+        if (request.method.toLowerCase() === "get") {
+           // show search
+           response.writeHead(200, commonHeaders);
+           renderer.view("header", {}, response);
+           renderer.view("search", {}, response);
+           renderer.view("footer", {}, response);
+           response.end();
+        } else {
+            // if url == "/" && POST
+            
+            // Get the post data from body
+            request.on("data", function(postBody) {
+                // Extract the username
+                var query = querystring.parse(postBody.toString());
+                // Redirect to /:username
+                response.writeHead(303, {"Location": "/" + query.username });
+                response.end();
+            });
+            
+        }
+        
      }
      
 }
